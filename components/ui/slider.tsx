f405a77a3aa16ffa0184a6ddd147b3ad56020397
mike laycock <laycock131@gmail.com>
2025-09-20 11:06:18 +0100

    UPDATE THE TEST

---
+++ components/ui/slider.tsx
@@ -0,0 +1,30 @@
+import * as React from "react"
+import * as SliderPrimitive from "@radix-ui/react-slider"
+import { cn } from "@/lib/utils"
+
+const Slider = React.forwardRef<
+  React.ElementRef<typeof SliderPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root> & { className?: string }
+>(({ className, ...props }, ref) => (
+  <SliderPrimitive.Root
+    ref={ref}
+    className={cn(
+      "relative flex w-full touch-none select-none items-center",
+      className
+    )}
+    {...props}
+  >
+    <SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-full bg-secondary">
+      <SliderPrimitive.Range className="absolute h-full bg-primary" />
+    </SliderPrimitive.Track>
+    <SliderPrimitive.Thumb
+      className={cn(
+        "block h-5 w-5 rounded-full border border-primary/40 bg-background shadow",
+        "focus:outline-none focus:ring-2 focus:ring-primary"
+      )}
+    />
+  </SliderPrimitive.Root>
+))
+Slider.displayName = "Slider"
+
+export { Slider }
